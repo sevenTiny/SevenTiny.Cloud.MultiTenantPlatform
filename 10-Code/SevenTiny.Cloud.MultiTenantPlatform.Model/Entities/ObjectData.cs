e101981f1e216e1f42a8bd7f2f76af9b385ee22e
wangdong3 <wangdong3@beisen.com>
2018-04-17 16:38:03 +0800

    initializa SevenTiny.Cloud.MultiTenantPlatform‘

---
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Model/Entities/ObjectData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SevenTiny.Cloud.MultiTenantPlatform.Model.Entities
+{
+    public class ObjectData
+    {
+        public MetaObject[] MetaObjects { get; set; }
+        public string MetaDataName { get; set; }
+        public string MetaDataId { get; set; }
+        /// <summary>
+        /// get MetaObject by propertyName which same as DataKey
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public object this[string propertyName]
+            => this.MetaObjectDictionary.ContainsKey(propertyName) ? this.MetaObjectDictionary[propertyName] : null;
+        private Dictionary<string, MetaObject> _MetaObjectDictionary { get; set; }
+        public Dictionary<string, MetaObject> MetaObjectDictionary
+        {
+            get
+            {
+                if (this._MetaObjectDictionary == null)
+                {
+                    this._MetaObjectDictionary = this.MetaObjects.ToDictionary(t => t.DataKey, v => v);
+                }
+                return this._MetaObjectDictionary;
+            }
+        }
+        public void Update(string propertyName, object metaDataValue)
+        {
+            if (this.MetaObjectDictionary.ContainsKey(propertyName))
+            {
+                this.MetaObjectDictionary[propertyName].DataValue = metaDataValue;
+            }
+        }
+        public void Updata(string propertyName, MetaObject metaObject)
+        {
+            if (this.MetaObjectDictionary.ContainsKey(propertyName))
+            {
+                this.MetaObjectDictionary[propertyName] = metaObject;
+            }
+        }
+    }
+}
---
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Model/Enums/DataType.cs
@@ -0,0 +1,19 @@
+﻿namespace SevenTiny.Cloud.MultiTenantPlatform.Model.Enums
+{
+    public enum DataType
+    {
+        Unknown = 0,
+        Number = 1,
+        Text = 2,
+        DateTime = 3,
+        Date = 4,
+        Boolean = 5,
+        Int = 6,
+        Long = 7,
+        Float = 8,
+        Double = 9,
+        RatingScale = 10,
+        StandradDate = 11,
+        StandradDateTime = 12
+    }
+}
