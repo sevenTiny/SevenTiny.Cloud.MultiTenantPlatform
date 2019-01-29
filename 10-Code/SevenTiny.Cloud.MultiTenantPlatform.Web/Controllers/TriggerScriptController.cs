0178ac0af39499c428d996f558b06bdc165f6de7
wangdong3 <wangdong3@beisen.com>
2019-01-29 14:52:44 +0800

    添加触发器脚本界面工具的相关文件:cup:

---
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Web/Controllers/TriggerScriptController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SevenTiny.Cloud.MultiTenantPlatform.Web.Controllers
+{
+    public class TriggerScriptController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
