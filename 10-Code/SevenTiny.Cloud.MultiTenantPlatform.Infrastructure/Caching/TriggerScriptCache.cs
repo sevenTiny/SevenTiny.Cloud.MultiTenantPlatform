03d28c404cd55ddcb25c742129deb8270a632617
wangdong3 <wangdong3@beisen.com>
2019-02-02 22:22:25 +0800

    change cache time

--- 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Infrastructure/Caching/TriggerScriptCache.cs
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Infrastructure/Caching/TriggerScriptCache.cs
@@ -11,11 +11,12 @@ namespace SevenTiny.Cloud.MultiTenantPlatform.Infrastructure.Caching
         public static TValue GetSet<TValue>(string script, Func<TValue> func)
         {
             var hashKey = script.GetHashCode();
+            var expired = TimeSpan.FromMinutes(15);
 
             var triggerScriptInCache = MemoryCacheHelper.Get<int, TValue>(hashKey);
             if (triggerScriptInCache == null)
             {
-                triggerScriptInCache = MemoryCacheHelper.Put<int, TValue>(hashKey, func(), TimeSpan.FromMinutes(30));
+                triggerScriptInCache = MemoryCacheHelper.Put<int, TValue>(hashKey, func(), expired);
             }
             return triggerScriptInCache;
         }
