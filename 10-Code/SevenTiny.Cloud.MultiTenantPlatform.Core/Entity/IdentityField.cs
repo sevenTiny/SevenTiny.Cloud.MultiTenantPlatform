ae4a77615ef9ee46cac6b2e56671cc833617fde4
lihaipeng <lihaipeng@beisen.com>
2019-03-07 21:52:40 +0800

    save

---
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Core/Entity/IdentityField.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MySqlX.XDevAPI.Relational;
+using SevenTiny.Bantina.Bankinate.Attributes;
+
+namespace SevenTiny.Cloud.MultiTenantPlatform.Core.Entity
+{
+    /// <summary>
+    /// 接口字段
+    /// </summary>
+    [Table]
+    public class IdentityField:MetaObjectManageInfo
+    {
+        public List<MetaField> MetaFields { get; set; }
+    }
+}
---
+++ 10-Code/SevenTiny.Cloud.MultiTenantPlatform.Core/ServiceContract/IIdentityFieldService.cs
@@ -0,0 +1,10 @@
+﻿using SevenTiny.Cloud.MultiTenantPlatform.Core.Entity;
+using SevenTiny.Cloud.MultiTenantPlatform.Core.Repository;
+
+namespace SevenTiny.Cloud.MultiTenantPlatform.Core.ServiceContract
+{
+    public interface IIdentityFieldService : ICommonInfoRepository<IdentityField>
+    {
+
+    }
+}
