2961f58c082dd4011800ff03db11fc3b15143215
seventiny <seventiny@foxmail.com>
2019-05-27 22:49:18 +0800

    移除旧版web和TriggerScriptEngine

--- 10-Code/SevenTiny.Cloud.MultiTenantPlatform.TriggerScriptEngine/Models/CountArg.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SevenTiny.Cloud.MultiTenantPlatform.TriggerScriptEngine.Models
-{
-    public class CountArg
-    {
-        public string operateCode { get; set; }
-        public int count { get; set; }
-    }
-}
