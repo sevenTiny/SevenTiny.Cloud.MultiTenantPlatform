3e88988329a4831042b663f42617b532c395b4cb
wangdong3 <wangdong3@beisen.com>
2019-06-04 15:43:15 +0800

    解决cookie未保存的问题:zap:

--- 10-Code/Seventiny.Cloud.DevelopmentWeb/Startup.cs
+++ 10-Code/Seventiny.Cloud.DevelopmentWeb/Startup.cs
@@ -29,7 +29,8 @@ namespace Seventiny.Cloud.DevelopmentWeb
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                //GDPR规定了cookie的操作方式，并且在用户同意使用cookie之前不会使用。
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
