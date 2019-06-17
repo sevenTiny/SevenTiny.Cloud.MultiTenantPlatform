74e300cb974a8ec48de721afc1bab9b6f5c9e153
wangdong3 <wangdong3@beisen.com>
2019-06-17 17:37:17 +0800

    add token

---
+++ 10-Code/SevenTiny.Cloud.Account/Controllers/Api/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SevenTiny.Cloud.Account.Models;
+
+namespace SevenTiny.Cloud.Account.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// login
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken(string userName, string pwd)
+        {
+            // push the user’s name into a claim, so we can identify the user later on.
+            var claims = new[]
+            {
+                    new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}") ,
+                    new Claim (JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddSeconds(100)).ToUnixTimeSeconds()}"),
+                    new Claim(ClaimTypes.Name, userName)
+                };
+            //sign the token using a secret key.This secret will be shared between your API and anything that needs to check that the token is legit.
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecurityKeyXXXX"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            //.NET Core’s JwtSecurityToken class takes on the heavy lifting and actually creates the token.
+            /**
+             * Claims (Payload)
+                Claims 部分包含了一些跟这个 token 有关的重要信息。 JWT 标准规定了一些字段，下面节选一些字段:
+
+                iss: The issuer of the token，token 是给谁的
+                sub: The subject of the token，token 主题
+                exp: Expiration Time。 token 过期时间，Unix 时间戳格式
+                iat: Issued At。 token 创建时间， Unix 时间戳格式
+                jti: JWT ID。针对当前 token 的唯一标识
+                除了规定的字段外，可以包含其他任何 JSON 兼容的字段。
+             * */
+            var token = new JwtSecurityToken(
+                issuer: "yourdomain.com",
+                audience: "yourdomain.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+
+
+}
--- 10-Code/SevenTiny.Cloud.Account/Startup.cs
+++ 10-Code/SevenTiny.Cloud.Account/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SevenTiny.Cloud.Account
 {
@@ -23,6 +26,22 @@ namespace SevenTiny.Cloud.Account
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //添加jwt验证：
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,//是否验证Issuer
+                        ValidateAudience = true,//是否验证Audience
+                        ValidateLifetime = true,//是否验证失效时间
+                        ClockSkew = TimeSpan.FromSeconds(30),
+                        ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                        ValidAudience = "yourdomain.com",//Audience
+                        ValidIssuer = "yourdomain.com",//Issuer，这两项和前面签发jwt的设置一致
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))//拿到SecurityKey
+                    };
+                });
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -37,6 +56,9 @@ namespace SevenTiny.Cloud.Account
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            ///add jwt
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
