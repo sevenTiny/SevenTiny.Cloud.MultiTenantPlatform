e8c197d7fb8b455215bac5a6a228eb94d6157f7d
seventiny <seventiny@foxmail.com>
2019-06-17 21:45:11 +0800

    移除空白行:zap:

--- 10-Code/SevenTiny.Cloud.Infrastructure/Const/MetaDataConst.cs
+++ 10-Code/SevenTiny.Cloud.Infrastructure/Const/MetaDataConst.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SevenTiny.Cloud.Infrastructure.Const
+﻿namespace SevenTiny.Cloud.Infrastructure.Const
 {
     /// <summary>
     /// 元数据常量
