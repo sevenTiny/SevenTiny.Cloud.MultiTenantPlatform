4b34b9a46c5de8be625e31cbd834037e653b374a
seventiny <seventiny@foxmail.com>
2020-03-29 11:23:24 +0800

    level up package

--- Code/SevenTiny.Cloud.MultiTenant.Infrastructure/Logging/Logger.cs
+++ Code/SevenTiny.Cloud.MultiTenant.Infrastructure/Logging/Logger.cs
@@ -7,18 +7,6 @@ namespace SevenTiny.Cloud.MultiTenant.Infrastructure.Logging
 {
     public static class Logger
     {
-        public static ILog logger = new LogManager();
-        public static void Debug(string log)
-        {
-            logger.Debug(log);
-        }
-        public static void Error(string log)
-        {
-            logger.Error(log);
-        }
-        public static void Info(string log)
-        {
-            logger.Info(log);
-        }
+        
     }
 }
